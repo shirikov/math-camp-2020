b690fb414dcd0f9909d928ef707284558a4a1ff4
Anton Shirikov <shirickov@gmail.com>
2020-08-04 09:50:14 -0500

    various minor changes

--- R/build.R
+++ R/build.R
@@ -5,16 +5,20 @@
 # ----------------------------------------------------
 
 # build slideshows;
+# first compile HTML files, then save as PDFs
 # if there are any issues installing or using pagedown,
 # can simply knit every individual RMD in RStudio and then
-# open resulting HTML files in Chrome and print them to PDF 
-# If chrome_print gives error (e.g., "Failed to generate output in 30 seconds"),
-# run the line again
+# open resulting HTML files in Chrome and print them to PDF; 
+# if chrome_print gives error, just run the line again
 library("pagedown")
 chrome_print("lectures/0-intro/lecture-0-intro.rmd")
-chrome_print("lectures/1-pre-calc/lecture-1-pre-calc.rmd")
+chrome_print("lectures/1-pre-calc/lecture-1-pre-calc.rmd",
+             timeout = 150)
 chrome_print("lectures/2-linear-algebra/lecture-2-linear-algebra.rmd")
 chrome_print("lectures/2-linear-algebra/lecture-2.5-R.rmd")
-chrome_print("lectures/3-calculus/lecture-3-calculus-day-1.rmd")
-chrome_print("lectures/3-calculus/lecture-3-calculus-day-2.rmd")
-chrome_print("lectures/4-probability/lecture-4-probability.rmd")
+chrome_print("lectures/3-calculus/lecture-3-calculus-day-1.rmd",
+             timeout = 150)
+chrome_print("lectures/3-calculus/lecture-3-calculus-day-2.rmd",
+             timeout = 150)
+chrome_print("lectures/4-probability/lecture-4-probability.rmd", 
+             timeout = 150)
